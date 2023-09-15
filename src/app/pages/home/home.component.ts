75a4021f4beb02ce8a5918737a6ded35636b2b29
Gilson Santos <gilson.santos@infobase.com.br>
2023-09-15 19:28:23 -0300

    refactor: ngfor e bug card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
+
+type content = {
+  id: string,
+  title: string,
+  description: string,
+  photoCover: string
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+
+  highLight: content = {
+    "id": "",
+    "title": "",
+    "description": "",
+    "photoCover": ""
+  };
+  contentList: content[] = [];
 
   constructor() { }
 
+
   ngOnInit(): void {
+    const clone = [...dataFake]
+    this.highLight = clone.shift() as content;
+    this.contentList = clone
   }
 
 }
