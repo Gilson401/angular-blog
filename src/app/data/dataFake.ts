9a2523aabffb2628e5afca29f8dd218e8c0bf514
Gilson Santos <gilson.santos@infobase.com.br>
2023-09-15 19:26:33 -0300

    refactor: ngFor para small cards e refct highligh

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,34 @@
+export const loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis hendrerit dolor magna eget est lorem ipsum. Sed lectus vestibulum mattis ullamcorper. Velit laoreet id donec ultrices tincidunt. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus. Cursus vitae congue mauris rhoncus. Cursus sit amet dictum sit. In cursus turpis massa tincidunt dui ut ornare lectus sit. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Mauris in aliquam sem fringilla ut morbi tincidunt augue.
+Sapien eget mi proin sed libero enim sed faucibus turpis. Fringilla urna porttitor rhoncus dolor. Amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Dolor sed viverra ipsum nunc aliquet bibendum enim. Felis imperdiet proin fermentum leo. Morbi non arcu risus quis. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Vestibulum sed arcu non odio euismod lacinia at. Elit ut aliquam purus sit amet luctus venenatis. Adipiscing commodo elit at imperdiet dui. At tempor commodo ullamcorper a lacus vestibulum sed arcu non. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Eget nunc scelerisque viverra mauris in aliquam. Cursus turpis massa tincidunt dui ut ornare lectus. Odio euismod lacinia at quis. Malesuada fames ac turpis egestas sed.
+Aliquet bibendum enim facilisis gravida neque convallis a cras. Eu sem integer vitae justo eget magna. Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Venenatis tellus in metus vulputate eu scelerisque felis. Dui sapien eget mi proin. Et magnis dis parturient montes nascetur ridiculus mus. Sapien eget mi proin sed libero enim sed. Tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Neque volutpat ac tincidunt vitae semper quis. Lacus vel facilisis volutpat est velit. Sagittis nisl rhoncus mattis rhoncus urna neque viverra. Rutrum quisque non tellus orci. Vitae proin sagittis nisl rhoncus mattis rhoncus.
+Dignissim sodales ut eu sem. At in tellus integer feugiat scelerisque varius morbi enim. Quam id leo in vitae. At consectetur lorem donec massa sapien faucibus et molestie ac. Leo a diam sollicitudin tempor id eu. Odio ut enim blandit volutpat maecenas volutpat. Imperdiet nulla malesuada pellentesque elit eget. Dictumst quisque sagittis purus sit amet volutpat. At erat pellentesque adipiscing commodo elit. Porttitor massa id neque aliquam vestibulum.
+Nulla facilisi nullam vehicula ipsum a arcu. Egestas congue quisque egestas diam in. Dui vivamus arcu felis bibendum ut. Feugiat in fermentum posuere urna nec tincidunt praesent semper. Diam in arcu cursus euismod quis viverra nibh. Turpis egestas maecenas pharetra convallis posuere morbi leo urna molestie. Neque sodales ut etiam sit amet nisl purus in mollis. Id volutpat lacus laoreet non curabitur gravida. Nullam eget felis eget nunc lobortis mattis. In arcu cursus euismod quis viverra. Turpis egestas integer eget aliquet nibh praesent. Venenatis urna cursus eget nunc scelerisque. Suspendisse interdum consectetur libero id. Interdum varius sit amet mattis vulputate. Sit amet consectetur adipiscing elit. Nunc consequat interdum varius sit. Maecenas ultricies mi eget mauris pharetra et ultrices neque. Quis enim lobortis scelerisque fermentum dui. Turpis egestas sed tempus urna et. Adipiscing enim eu turpis egestas.`;
+
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "1",
+    "title": "Novo Homem de Ferro em 3D",
+    "description": loremText,
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
-    "id":"2",
+    "id": "2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "description": loremText,
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id": "3",
+    "description": loremText,
+    "title": "Novo filme do Pantera Negra será  lançado em breve",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "4",
+    "description": loremText,
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
+
+
